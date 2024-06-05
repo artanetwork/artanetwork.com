6fa12929302f3020775ab2a8d526ce147aa67cd6
Omid Shojaee <omid.shojaee@gmail.com>
2024-06-05 16:08:32 +0330

    Translations

---
+++ core/translations/filebrowser.py
@@ -0,0 +1,9 @@
+from django.utils.translation import gettext
+
+gettext('Rotate 180°')
+gettext('Actions')
+gettext('FileBrowser')
+gettext('Permission denied.')
+gettext(
+    'The Name will be normalized by converting or stripping all non-standard characters.'
+)
